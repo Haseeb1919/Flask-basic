36031bb90fe1e5aae5b7f791eb6f71eff7425f6f
Haseeb1919 <haseeburrehman1919@gmail.com>
2023-08-03 01:20:16 +0500

    inheritance and bootstrap tested

--- chapter 3/chp_3_bootstrap_templates_inheritance.py
+++ chapter 3/chp_3_bootstrap_templates_inheritance.py
@@ -1,23 +1,21 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/test1")
-def test1():
-    return render_template("test1.html")
+@app.route("/test")
+def test():
+    return render_template("testing.html")
 
 @app.route("/test2")
 def test2():
-    return render_template("test2.html")
-
-@app.route("/test3")
-def test3():
-    return render_template("test3.html")
-
+    return render_template("testing2.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)     
+
+  
