43f1f3b72099c26eb9e6d2510367c87fe0eca3f2
Haseeb1919 <haseeburrehman1919@gmail.com>
2023-08-02 21:47:34 +0500

    routing update

--- routing.py
+++ routing.py
@@ -2,15 +2,16 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-#routing different pages 
+#defining the home page
 @app.route("/")
 def home():
     return "Hello, Flask!"
-
+#defining a new page
 @app.route("/user") 
 def user():
     return "<h1> Hello, User! </h1>"
 
+#passing in a variable
 #what user passes in the url will be passed to the function as an argument and returned
 @app.route("/<member>")
 def member(member):
@@ -20,12 +21,17 @@ def member(member):
 @app.route("/<int:member_id>")
 def member_id(member_id):
     return f"Member ID: {member_id}"
-    
+
 #redirecting to another page    
 @app.route("/admin")
 def admin():
     #defing the function to redirect to in return
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name="Admin!"))
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
