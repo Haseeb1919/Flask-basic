034c815a7ca623a1c4c208e6c98e034765480596
Haseeb1919 <haseeburrehman1919@gmail.com>
2023-08-03 19:19:47 +0500

    'updated session'

--- chapter 5/chp_5_session.py
+++ chapter 5/chp_5_session.py
@@ -1,9 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 
 #secret key is used to encrypt the session data
 app.secret_key = "haseeb"
+#storing the permanent session data 
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 # The route() decorator tells Flask what URL should trigger the function.
 @app.route("/")
@@ -15,12 +18,16 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if (request.method == "POST"):
+        session.permanent = True
         user = request.form["username"]
         #session is a dictionary that stores data across requests
         session["user"] = user
         return redirect(url_for("user"))
     else:
-       return render_template("login.html")
+        if "user" in session:
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
 
 
 #checking the session is created or not
@@ -33,7 +40,11 @@ def user():
         return redirect(url_for("login"))
 
 
-
+#deleting the session
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if (__name__ == "__main__"):
     app.run(debug=True)
