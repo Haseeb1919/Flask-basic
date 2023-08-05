efff3f97afe9206cc220f2b15d249ab5f9583c01
Haseeb1919 <haseeburrehman1919@gmail.com>
2023-08-06 00:10:38 +0500

    message flashing

---
+++ chapter 6/chp_6_msg_flashing.py
@@ -0,0 +1,50 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+app = Flask(__name__)
+
+#secret key is used to encrypt the session data
+app.secret_key = "haseeb"
+#storing the permanent session data 
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+# The route() decorator tells Flask what URL should trigger the function.
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# GET and POST are the two types of HTTP requests.
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if (request.method == "POST"):
+        session.permanent = True
+        user = request.form["username"]
+        #session is a dictionary that stores data across requests
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+
+#checking the session is created or not
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+
+#deleting the session
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
